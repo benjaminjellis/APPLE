e7f5b0533446cc65824054b7089147719c8f8236
Benjamin Ellis <benellisuk@gmail.com>
2020-06-26 22:29:30 +0100

    adding some elements for updated readme

--- utilities/convertups.py
+++ utilities/convertups.py
@@ -5,6 +5,6 @@ Script used to convert xlsx to csv
 import pandas as pd
 
 
-df = pd.read_excel("~/Downloads/week31up.xlsx")
-df.to_csv("~/Downloads/week31up.csv", index= False, index_label = False)
+df = pd.read_excel("~/Downloads/week32up.xlsx")
+df.to_csv("~/Downloads/week32up.csv", index= False, index_label = False)
 
