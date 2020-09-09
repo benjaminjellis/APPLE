b803a099eb31f71e7229903d2455a453c0c575a1
Benjamin Ellis <benellisuk@gmail.com>
2020-09-09 22:53:59 +0100

    updated, refactored and tested results.py analytics

--- analytics/TABEL_TEST.PY.py
+++ analytics/TABEL_TEST.PY.py
@@ -1,20 +1,3 @@
-import pandas as pd
-
-
-d = {"Name": ["A", "B", "C", "D", "E", "F", "G"],
-     "Result": [0.9, 0.8, 0.55, 0.67, 0.9, 0.4, 0.35]}
-
-df = pd.DataFrame(data = d)
-
-
-def winners_from_dataframe(dataframe):
-    winners_str_to_return = ""
-    max_val = dataframe["Result"].max()
-    winners = dataframe[dataframe["Result"] == max_val]["Name"].to_list()
-    for winner in winners:
-        winners_str_to_return += winner + ", "
-    return winners_str_to_return
-
-
-winner_str = winners_from_dataframe(dataframe = df)
-print(winner_str)
+winners = ["A", "B", "C"]
+winners_str = ','.join(winners)
+print("This weeks' winner(s) are: {}".format(winners_str))
