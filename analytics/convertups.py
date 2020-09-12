4a3952e925d5d46967dd46a8a3df5048c0b7c749
Benjamin Ellis <benellisuk@gmail.com>
2020-09-12 11:48:58 +0100

    first run of the season, changes to mining to get it wokring. These will end up being merged.

--- analytics/convertups.py
+++ analytics/convertups.py
@@ -5,9 +5,9 @@ Script used to convert xlsx to csv
 import pandas as pd
 from core.miners import team_cleaner
 
-df = pd.read_excel("~/Downloads/week35up.xlsx")
+df = pd.read_excel("~/Downloads/week1userpredictions.xlsx")
 
 df["HomeTeam"] = df.apply(lambda x: team_cleaner(x["HomeTeam"]), axis = 1)
 df["AwayTeam"] = df.apply(lambda x: team_cleaner(x["AwayTeam"]), axis = 1)
-df.to_csv("~/Downloads/week35up.csv", index= False, index_label = False)
+df.to_csv("~/Downloads/week1userpredictions.csv", index= False, index_label = False)
 
