126eaf7a6bd53986d54c293850016f1c6344eaf5
Benjamin Ellis <benellisuk@gmail.com>
2020-09-16 17:39:49 +0100

    adding week2 fixtures ahead of time, some testing on team cleaner function. All ok to merge

--- analytics/convertups.py
+++ analytics/convertups.py
@@ -5,7 +5,7 @@ Script used to convert xlsx to csv
 import pandas as pd
 from core.miners import team_cleaner
 
-df = pd.read_excel("~/Downloads/week1userpredictions.xlsx")
+df = pd.read_excel("~/Downloads/week2userpredictions.xlsx")
 
 df["HomeTeam"] = df.apply(lambda x: team_cleaner(x["HomeTeam"]), axis = 1)
 df["AwayTeam"] = df.apply(lambda x: team_cleaner(x["AwayTeam"]), axis = 1)
