c79b1af6dbdcacc157ac3c7cd9da41de7869ee1a
Benjamin Ellis <benellisuk@gmail.com>
2020-09-26 19:49:08 +0100

    removed a test file

--- test.py
+++
@@ -1,4 +0,0 @@
-
-from analytics.visulisation import Visualisation
-vis = Visualisation("data/aggregated_results/20_21/predictions_and_results_log.csv")
-vis.stratified_performance(metric = "top 6 teams")
