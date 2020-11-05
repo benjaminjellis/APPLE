565c55018fff69b459e95f87e09a5f2d0f745469
Benjamin Ellis <benellisuk@gmail.com>
2020-11-05 18:49:22 +0000

    Migrated all predictions to STRUDEL

--- test.py
+++
@@ -1,9 +0,0 @@
-from analytics.results import Results, Predictions
-from analytics.visulisation import Visualisation
-
-Results(user_predictions = "data/user_predictions/20_21/week7/week7userpredictions.csv",
-        apple_predictions =
-        "results/20_21/week7/7640a728-04fb-11eb-8c32-acde48001122_predicted_results.csv").aggregate(
-        ftrs = "data/ftrs/20_21/week7/ftrs.csv",
-        winners_log = "data/aggregated_results/20_21/winners_log.csv",
-        aggregated_results_file = "data/aggregated_results/20_21/predictions_and_results_log.csv")
